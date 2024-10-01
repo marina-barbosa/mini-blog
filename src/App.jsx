f4e0b1ce2eb4b4b03274a334300ca5222f31d483
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-10-01 15:54:20 -0300

    feat: Passando o CurrentUser para o Context

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,39 @@ import { Navbar } from "./components/navbar"
 import { Footer } from "./components/footer"
 import { Register } from "./pages/register/register"
 import { Login } from "./pages/login/login"
+import { onAuthStateChanged } from "firebase/auth"
+
+import { useState, useEffect } from "react"
+import { useAuthentication } from "./hooks/useAuthentication"
 
 import { AuthProvider } from "./context/AuthContext"
 
 function App() {
 
+  const [currentUser, setCurrentUser] = useState(undefined)
+  // Função onAuthStateChanged do Firebase monitorar mudanças na autenticação do usuário
+  // Obtém o objeto 'auth' do hook 'useAuthentication', que será usado para verificar o estado de autenticação
+  const { auth } = useAuthentication()
+
+  const loadingUser = currentUser === undefined
+  // useEffect para monitorar as mudanças no estado de autenticação.
+  // Quando o componente é montado ou o valor de 'auth' muda, ele chama 'onAuthStateChanged'
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      // Atualiza o estado 'user' com as informações do usuário logado, ou null se não estiver logado
+      setCurrentUser(currentUser)
+    })
+  }, [auth])
+
+  if (loadingUser) {
+    return (
+      <p>Carregando...</p> // TODO: add gif de carregamento
+    )
+  }
+  // else
   return (
     <div className="font-urbanist antialiased bg-gray-100 text-gray-600 flex flex-col min-h-screen">
-      <AuthProvider>
+      <AuthProvider value={{ currentUser }}>
         <BrowserRouter>
           <Navbar />
           <main className="flex flex-grow p-8">
