823320e5fd7f5583d1085fef0a0452f232a0f3e8
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-10-02 00:31:10 -0300

    feat: page about

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom"
+
 export const About = () => {
   return (
-    <h1>About</h1>
+    <div className="text-center text-gray-500 mx-auto">
+      <h2 className="text-2xl font-semibold">Sobre o Mini <span className="font-bold">Blog</span></h2>
+      <p className="mt-8 mb-11">Este projeto consiste em um blog desenvolvido com React no front-end e Firebase no back-end.</p>
+      <Link to="/posts/create" className="btn-primary">Criar post</Link>
+    </div>
   )
 }
